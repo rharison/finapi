22ee2fdd7eef1f9163317b8d39e0cd357d5fd0b5
rharison <rharison.abreu@gmail.com>
2022-03-17 15:39:31 -0300

    Adicionando funcao de remover conta

--- src/index.js
+++ src/index.js
@@ -108,4 +108,12 @@ app.put('/account', verifyIfExistsAccountCPF, (request, response) => {
   return response.status(201).send();
 });
 
+app.delete('/account', verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
+
+  customers.splice(customer, 1);
+
+  return response.status(200).json(customers);
+});
+
 app.listen(3333);
