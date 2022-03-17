44a12363dedea2cb4c0b4d7d2a1afa20b9c114fc
rharison <rharison.abreu@gmail.com>
2022-03-17 15:11:11 -0300

    Adicionando função de saque

--- src/index.js
+++ src/index.js
@@ -14,6 +14,17 @@ function verifyIfExistsAccountCPF(request, response, next) {
   return next();
 }
 
+function getBalance(statement) {
+  const balance = statement.reduce((acc, operation) => {
+    if (operation.type === 'credit') {
+      return acc + operation.amount;
+    } else {
+      return acc - operation.amount;
+    }
+  }, 0);
+  return balance;
+}
+
 app.post('/account', (request, response) => {
   const { cpf, name } = request.body;
   const customerAlreadyExists = customers.some(
@@ -42,6 +53,25 @@ app.post('/deposit', verifyIfExistsAccountCPF, (request, response) => {
     type: 'credit',
   };
   customer.statement.push(statementOperation);
+  return response.status(201).send();
+});
+
+app.post('/withdraw', verifyIfExistsAccountCPF, (request, response) => {
+  const { amount } = request.body;
+  const { customer } = request;
+  const balance = getBalance(customer.statement);
+
+  if (balance < amount) {
+    return response.status(400).json({ error: 'Insufficient funds!' });
+  }
+
+  const statementOperation = {
+    amount,
+    created_at: new Date(),
+    type: 'debit',
+  };
+  customer.statement.push(statementOperation);
+
   return response.status(201).send();
 });
 
