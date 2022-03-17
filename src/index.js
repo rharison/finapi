96e81062a13f6f3254b50a980ccff7b00f2ac477
rharison <rharison.abreu@gmail.com>
2022-03-17 14:46:53 -0300

    Adicionando func. criar conta e gerar extrato

---
+++ src/index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const { v4: uuidv4 } = require('uuid');
+const app = express();
+app.use(express.json());
+const customers = [];
+
+function verifyIfExistsAccountCPF(request, response, next) {
+  const { cpf } = request.headers;
+  const customer = customers.find((customer) => customer.cpf === cpf);
+  if (!customer) {
+    return response.status(400).json({ error: 'Customer not found.' });
+  }
+  request.customer = customer;
+  return next();
+}
+
+app.post('/account', (request, response) => {
+  const { cpf, name } = request.body;
+  const customerAlreadyExists = customers.some(
+    (customer) => customer.cpf === cpf,
+  );
+  if (customerAlreadyExists) {
+    return response.status(400).json({ error: 'Customer already exists!' });
+  }
+
+  customers.push({
+    cpf,
+    name,
+    id: uuidv4(),
+    statement: [],
+  });
+  return response.status(201).send();
+});
+
+app.get('/statement', verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
+  return response.json(customer.statement);
+});
+
+app.listen(3333);
